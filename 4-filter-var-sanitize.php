b22bf04aa9a4508cedff453de553948dbd9931b5
unknown <codeleap@outlook.com>
2019-03-08 13:35:33 +0100

    Comments updated on examples number 4.

--- 4-filter-var-sanitize.php
+++ 4-filter-var-sanitize.php
@@ -6,10 +6,6 @@
 * See 4-pdo-prepared-statements.php. ALWAYS use prepared statements after validation and sanitization.
 */
 
-// Any parameters you send when using a prepared statement will just be treated as strings
-// MySQL extension, which was deprecated in PHP 5.5.0 and removed entirely in PHP 7.0.0.
-// mysql_query("INSERT INTO `table` (`column`) VALUES ('$unsafe_variable')");
-
 function test_string_filters($str){
    var_dump(filter_var($str, FILTER_SANITIZE_STRING));
    var_dump(filter_var($str, FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES));
